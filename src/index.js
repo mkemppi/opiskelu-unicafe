09b15d24d50f67c6ad5576d77329b165acdfa918
Mikko Kemppi <mikko.kemppi@tahko.com>
2020-08-25 22:28:13 +0300

    step 3

--- src/index.js
+++ src/index.js
@@ -7,12 +7,28 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = ({ good, neutral, bad }) => {
+
+  const vastauksia = good + neutral + bad;
+
+  return (
+    <div>
+    <h3>Tilastot</h3>
+    Hyviä palautteita <b>{good}</b><br/>
+    Neutraaleja palautteita <b>{neutral}</b><br/>
+    Huonoja palautteita <b>{bad}</b><br/>
+    Palautteita yhteensä <b>{vastauksia}</b><br/>
+    Keskiarvo <b>{((good * 1) + (bad * -1))/vastauksia }</b><br/>
+    Positiivia palautteita <b>{ (good / vastauksia) * 100 }%</b><br/>
+    </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const vastauksia = good + neutral + bad;
 
   return (
 
@@ -21,13 +37,8 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="HYVÄ" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="NEUTRAALI" />
       <Button handleClick={() => setBad(bad + 1)} text="HUONO" />
-      <h3>Tilastot</h3>
-      Hyviä palautteita <b>{good}</b><br/>
-      Neutraaleja palautteita <b>{neutral}</b><br/>
-      Huonoja palautteita <b>{bad}</b><br/>
-      Palautteita yhteensä <b>{vastauksia}</b><br/>
-      Keskiarvo <b>{((good * 1) + (bad * -1))/vastauksia }</b><br/>
-      Positiivia palautteita <b>{ (good / vastauksia) * 100 }%</b><br/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+
     </div>
   )
 }
