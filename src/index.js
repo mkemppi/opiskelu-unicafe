79adb17c9cb54a52504822c6497d66fd9953941a
Mikko Kemppi <mikko.kemppi@tahko.com>
2020-08-25 23:01:00 +0300

    step 6

--- src/index.js
+++ src/index.js
@@ -12,37 +12,34 @@ const StatisticsLine = (props) => {
   const vastauksia = good + neutral + bad;
   if(props.text==="good") {
     return (
-      <div>Hyviä palautteita <b>{good}</b></div>      
+      <tr><td>Hyviä palautteita</td><td><b>{good}</b></td></tr>      
     )
   }
   if(props.text==="neutral") {
     return (
-      <div>Neutraaleja palautteita <b>{neutral}</b></div>      
+      <tr><td>Neutraaleja palautteita</td><td><b>{neutral}</b></td></tr>      
     )
   }
   if(props.text==="bad") {
     return (
-      <div>Huonoja palautteita <b>{bad}</b></div>      
+      <tr><td>Huonoja palautteita</td><td><b>{bad}</b></td></tr>      
     )
   }
   if(props.text==="total") {
     return (
-      <div>Palautteita yhteensä <b>{vastauksia}</b></div>      
+      <tr><td>Palautteita yhteensä</td><td><b>{vastauksia}</b></td></tr>      
     )
   }
   if(props.text==="average") {
     return (
-      <div>Keskiarvo <b>{((good * 1) + (bad * -1))/vastauksia }</b></div>      
+      <tr><td>Keskiarvo</td><td><b>{Number(((good * 1) + (bad * -1))/vastauksia).toFixed(1) }</b></td></tr>      
     )
   }
   if(props.text==="positive") {
     return (
-      <div>Positiivisia palautteita <b>{ (good / vastauksia) * 100 }%</b></div>      
+      <tr><td>Positiivisia palautteita</td><td><b>{ Number((good / vastauksia) * 100).toFixed(1) }%</b></td></tr>      
     )
   }
-  return (
-    <div></div>
-  )
 
 }
 
@@ -55,12 +52,16 @@ const Statistics = (props) => {
     return (
       <div>
       <h3>Tilastot</h3>
+      <table>
+        <tbody>
       <StatisticsLine text="good" value={props} />
       <StatisticsLine text="neutral" value={props} />
       <StatisticsLine text="bad" value={props} />
       <StatisticsLine text="total" value={props} />
       <StatisticsLine text="average" value={props} />
       <StatisticsLine text="positive" value={props} />
+        </tbody>
+      </table>
       </div>
     )
   }
